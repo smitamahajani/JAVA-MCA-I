1f55b1c0abfa2d9fd3c06c7e60f38bb45e5eb0b4
smitamahajani <161463137+smitamahajani@users.noreply.github.com>
2024-03-05 11:33:24 +0530

    Add files via upload

---
+++ TextAreaExample.java
@@ -0,0 +1,17 @@
+import javax.swing.*;  
+public class TextAreaExample  
+{  
+     TextAreaExample(){  
+        JFrame f= new JFrame();  
+        JTextArea area=new JTextArea("Welcome to  TextAreaExample  ");  
+        area.setBounds(10,30, 200,200);  
+        f.add(area);  
+        f.setSize(300,300);  
+        f.setLayout(null);  
+        f.setVisible(true);  
+	f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+     }  
+public static void main(String args[])  
+    {  
+   new TextAreaExample();  
+    }}  
