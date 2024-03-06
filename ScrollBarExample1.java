74087923280bdcde02144f1433ec3f2c1468d191
smitamahajani <161463137+smitamahajani@users.noreply.github.com>
2024-03-06 16:16:45 +0530

    Add files via upload

---
+++ ScrollBarExample1.java
@@ -0,0 +1,26 @@
+import javax.swing.*;  
+import java.awt.event.*;  
+class ScrollBarExample1  
+{  
+ScrollBarExample1(){  
+    JFrame f= new JFrame("Scrollbar Example");  
+    final JLabel label = new JLabel();          
+    label.setHorizontalAlignment(JLabel.CENTER);    
+    label.setSize(400,100);  
+    final JScrollBar s=new JScrollBar();  
+    s.setBounds(100,100, 20,100);  
+    f.add(s); f.add(label);  
+    f.setSize(400,400);  
+   f.setLayout(null);  
+   f.setVisible(true);  
+	f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+   s.addAdjustmentListener(new AdjustmentListener() {  
+    public void adjustmentValueChanged(AdjustmentEvent e) {  
+       label.setText("Vertical Scrollbar value is:"+ s.getValue());  
+    }  
+ });  
+}  
+public static void main(String args[])  
+{  
+   new ScrollBarExample1();  
+}}
