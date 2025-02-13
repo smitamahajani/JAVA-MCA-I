35ce995d9b222094e6035ea9572f2c98460aa529
smitamahajani <161463137+smitamahajani@users.noreply.github.com>
2025-02-13 13:33:31 +0530

    Update ButtonExample2.java

--- ButtonExample2.java
+++ ButtonExample2.java
@@ -4,7 +4,7 @@ public class ButtonExample2
 ButtonExample2()
 {    
 JFrame f=new JFrame("Button Example");            
-//JButton b=new JButton(new ImageIcon("D:\\icon.png"));
+//Add your device image path over here
 JButton b=new JButton(new ImageIcon("C:\\Screenshot 2023-12-15 135730.png"));    
 b.setBounds(500,500,500, 540);    
 f.add(b);    
